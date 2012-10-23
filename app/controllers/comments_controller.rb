adef29987a5619abb3578ed9526890eb59bf6f05
agampa <gampaalekhya@gmail.com>
2012-10-22 20:08:11 -0400

    Updated Site

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,15 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     comment = @post.comments.create(params[:comment])
-    #redirect_to post_path(@post)
-    render_text "<li>" + params[:comment] + "</li>"
+    redirect_to post_path(@post)
+    #render_text "<li>" + params[:comment] + "</li>"
   end
+  
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+  
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,5 @@ class Post < ActiveRecord::Base
   validates :title, :presence => true, :length => {:minimum => 5}
   validates :context, :presence => true, :length => {:minimum => 10}
   
-  has_many :comments
+  has_many :comments, :dependent => :destroy
 end
